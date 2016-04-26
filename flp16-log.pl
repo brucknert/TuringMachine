5f325566f8dae17bef11d7442e38498383c6db65
Tomas Bruckner <tomasbruckner@seznam.cz>
2016-04-26 20:41:38 +0200

    refaktor

--- flp16-log.pl
+++ flp16-log.pl
@@ -4,110 +4,129 @@
 % Date:	   2016-04-26
 %
 
-readLines(LL) :-
-    readLine(L,C),
-    ( isEOFEOL(C), LL = [];
-    readLines(LLL), LL = [L|LLL]).
+% Reads every line from stdin
+readLines(Lines) :-
+    readLine(Line,Char),
+    ( isEOFEOL(Char), Lines = [];
+    readLines(RestLines), Lines = [Line|RestLines]).
 
-readLine(L, C) :-
-    get_char(C),
-    (isEOFEOL(C), L = [], !;
-     readLine(LL,_), [C|LL] = L
-    ).    
-
-isEOFEOL(C) :-
-    C == end_of_file;
-    (char_code(C,Code), Code==10).
-
-% Parse all lines
-parseLines([],[]).
-parseLines([H|L], [H2|P]) :-
-parseLines(L,P), parseLine(H,H2).
-
-% Parse single line
-% tape "symbolsymbolsymbolsymbol"
-% transition "oldstate symbol newstate action"
-parseLine(L,P) :- 
-    nth0(0, L, OS),
-    nth0(2, L, S),
-    nth0(4, L, NS),
-    nth0(6, L, A),
-    P = [OS, S, NS, A].
-parseLine(L,P) :- P = L.
-
-% Tape, State, Transitions, Result
-simulateTS(T, S, Trs, R) :-
-    S == 'F', R = [], true;
-    getTransition(T, S, Trs, Tr),
-    nth0(2, Tr, NS),
-    nth0(3, Tr, A), 
-    ( A == 'L', shiftLeft(T, S, NT);
-      A == 'R', shiftRight(T, S, NT);
-      writeSymbol(T, S, A, NT)
-    ),
-    changeState(NT, S, NS, NTT),
-    simulateTS(NTT, NS, Trs, R2),
-    R = [NTT|R2].
-
-% Tape, State, Transitions, Transition
-getTransition(T, S, [H|Trs], Tr) :-
-    nth0(I, T, S),
-    J is I + 1,
-    nth0(J, T, A),
-    (nth0(0, H, X), X == S, 
-     nth0(1, H, Y), Y == A, Tr = H;
-     getTransition(T, S, Trs, Tr)
+% Reads single line
+readLine(Line, Char) :-
+    get_char(Char),
+    (isEOFEOL(Char), Line = [], !;
+     readLine(RestChars,_), [Char|RestChars] = Line
     ).
 
-changeState(T, S, NS, NT) :-
-    nth0(I, T, S), replace(T, I, NS, NT).
+% Checks for EOF or EOL
+isEOFEOL(Char) :-
+    Char == end_of_file;
+    (char_code(Char,Code), Code == 10).
 
-% http://stackoverflow.com/questions/8519203/prolog-replace-an-element-in-a-list-at-a-specified-index
-replace([_|T], 0, X, [X|T]).
-replace([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace(T, NI, X, R), !.
-replace(L, _, _, L).
+% Parses every lines to tape format or transition format
+parseLines([],[]).
+parseLines([Line|RestLines], [ParsedLine|ParsedLines]) :-
+    parseLines(RestLines,ParsedLines), parseLine(Line,ParsedLine).
 
-writeSymbol(T, S, A, R) :-
-    nth0(I, T, S),
-    J is I + 1,
-    replace(T, J, A, R).
+% Parses single line
+% tape "symbolsymbolsymbolsymbol"
+% transition "oldstate symbol newstate action"
+parseLine(Line,Parsed) :-
+    nth0(0, Line, OldState),
+    nth0(2, Line, Symbol),
+    nth0(4, Line, NewState),
+    nth0(6, Line, Action),
+    Parsed = [OldState, Symbol, NewState, Action].
+parseLine(Line,Parsed) :- Parsed = Line.
 
-shiftLeft(T, S, R) :- 
-    nth0(I, T, S),
-    J is I - 1,
-    (J < 0, writef("Abnormal halt!\n"),halt;
-     nth0(J, T, Tmp),
-     replace(T, J, S, X),
-     replace(X, I, Tmp, R)
-    ).   
+% Simulates Turing Machine
+simulateTS(Tape, State, Transitions, Output) :-
+    State == 'F', Output = [];
 
-writeConfigurations([H|T]) :-
-    writef("%s\n", [H]), writeConfigurations(T).
-writeConfigurations([]).
-
-% check na blank
-shiftRight(T, S, R) :- 
-    nth0(I, T, S),
-    J is I + 1,
-    length(T, Len),
-    Len2 is Len - 1,
-    (J == Len2, append(T, [' '], TT);
-     TT = T
+    getTransition(Tape, State, Transitions, SingleTransition),
+    nth0(2, SingleTransition, NewState),
+    nth0(3, SingleTransition, Action),
+    ( Action == 'L', shiftLeft(Tape, State, NewTape);
+      Action == 'R', shiftRight(Tape, State, NewTape);
+      writeSymbol(Tape, State, Action, NewTape)
     ),
-    nth0(J, TT, Tmp),
-    replace(TT, J, S, X),
-    replace(X, I, Tmp, R).
+    changeState(NewTape, State, NewState, FNewTape),
+    simulateTS(FNewTape, NewState, Transitions, RestOutput),
+    Output = [FNewTape|RestOutput].
 
-main :- 
+% Gets transition for current state and symbol on head
+getTransition(Tape, State, [CurrentTransition|Transitions], OutputTransition) :-
+    nth0(StateIndex, Tape, State),
+    SymbolIndex is StateIndex + 1,
+    nth0(SymbolIndex, Tape, Symbol),
+    (nth0(0, CurrentTransition, TransitionState),
+     TransitionState == State,
+     nth0(1, CurrentTransition, TransitionSymbol),
+     TransitionSymbol == Symbol,
+     OutputTransition = CurrentTransition ;
+
+     getTransition(Tape, State, Transitions, OutputTransition)
+    ).
+
+% Changes state on tape
+changeState(Tape, OldState, NewState, NewTape) :-
+    nth0(StateIndex, Tape, OldState), replace(Tape, StateIndex, NewState, NewTape).
+
+% Replaces element in list at specified index
+% http://stackoverflow.com/questions/8519203/prolog-replace-an-element-in-a-list-at-a-specified-index
+replace([_|RestElements], 0, NewElement, [NewElement|RestElements]).
+replace([FirstElement|RestOriginalElements], Index, NewElement, [FirstElement|RestElements]):- 
+    Index > -1,
+    NewIndex is Index - 1,
+    replace(RestOriginalElements, NewIndex, NewElement, RestElements), !.
+replace(List, _, _, List).
+
+% Writes speicified symbol on tape at head location
+writeSymbol(Tape, State, Symbol, NewTape) :-
+    nth0(StateIndex, Tape, State),
+    SymbolIndex is StateIndex + 1,
+    replace(Tape, SymbolIndex, Symbol, NewTape).
+
+% Shifts head to the left
+shiftLeft(Tape, State, NewTape) :-
+    nth0(StateIndex, Tape, State),
+    NewStateIndex is StateIndex - 1,
+    (NewStateIndex < 0, writef("Abnormal halt!\n"),halt;
+     nth0(NewStateIndex, Tape, Tmp),
+     replace(Tape, NewStateIndex, State, TmpTape),
+     replace(TmpTape, StateIndex, Tmp, NewTape)
+    ).
+
+% Shifts head to the right
+shiftRight(Tape, State, NewTape) :-
+    nth0(StateIndex, Tape, State),
+    NewStateIndex is StateIndex + 1,
+    length(Tape, Len),
+    HighestIndex is Len - 1,
+    (NewStateIndex == HighestIndex, append(Tape, [' '], TmpTape);
+     TmpTape = Tape
+    ),
+    nth0(NewStateIndex, TmpTape, Tmp),
+    replace(TmpTape, NewStateIndex, State, TmpTape2),
+    replace(TmpTape2, StateIndex, Tmp, NewTape).
+
+% Writes configurations after simmulation
+writeConfigurations([]).
+writeConfigurations([Configuration|RestConfigurations]) :-
+    writef("%s\n", [Configuration]), writeConfigurations(RestConfigurations).
+
+% Starting point, parses Turing Machine from stdin
+% TS format:
+% <state><space><symbol><space><newstate><space><symbol/L/R>
+% <symbol><symbol><symbol>...
+main :-
     prompt(_,''),
-    readLines(LL),
-    parseLines(LL, LP),
-    last(LP,T),
-    delete(LP,T,Trs),
-    append(['S'], T, ST),
+    readLines(Lines),
+    parseLines(Lines, LinesParsed),
+    last(LinesParsed,Tape),
+    delete(LinesParsed,Tape,Transitions),
+    append(['S'], Tape, StartingTape),
     !,
-    simulateTS(ST, 'S', Trs, R),
-    writeConfigurations(R).
+    simulateTS(StartingTape, 'S', Transitions, Configurations),
+    writeConfigurations(Configurations).
 
 % vim: expandtab:shiftwidth=4:tabstop=4:softtabstop=0:textwidth=120
-
