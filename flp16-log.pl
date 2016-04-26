09e13176d2221601f4956b404aebeb7d20b1be0f
Tomas Bruckner <xbruck02@stud.fit.vutbr.cz>
2016-04-26 17:04:39 +0200

    nefunguje to

--- flp16-log.pl
+++ flp16-log.pl
@@ -12,18 +12,17 @@ readLines(LL) :-
 readLine(L, C) :-
     get_char(C),
     (isEOFEOL(C), L = [], !;
-        readLine(LL,_), 
-        [C|LL] = L).    
+     readLine(LL,_), [C|LL] = L
+    ).    
 
 isEOFEOL(C) :-
     C == end_of_file;
     (char_code(C,Code), Code==10).
 
-
 % Parse all lines
 parseLines([],[]).
 parseLines([H|L], [H2|P]) :-
-    parseLines(L,P), parseLine(H,H2).
+parseLines(L,P), parseLine(H,H2).
 
 % Parse single line
 % tape "symbolsymbolsymbolsymbol"
@@ -34,15 +33,69 @@ parseLine(L,P) :-
     nth0(4, L, NS),
     nth0(6, L, A),
     P = [OS, S, NS, A].
+parseLine(L,P) :- P = L.
 
-parseLine(L,P) :- atom_chars(P, L).
+% Tape, State, Transitions, Result
+simulateTS(T, S, Trs, R) :-
+    S == 'F', true;
+    getTransition(T, S, Trs, Tr),
+    nth0(3, Tr, NS),
+    nth0(4, Tr, A), 
+    ( A == 'L', shiftLeft(T, S, NT);
+      A == 'R', shiftRight(T, S, NT);
+      writeSymbol(T, S, A, NT)
+    ),
+    simulateTS(NT, NS, Trs, R2),
+    R = [NT|R2].
+
+% Tape, State, Transitions, Transition
+getTransition(T, S, [H|Trs], Tr) :-
+    nth0(I, T, S),
+    J is I + 1,
+    nth0(J, T, A),
+    (nth0(0, H, X), X == S, 
+     nth0(1, H, Y), Y == A, Tr = H;
+     getTransition(T, S, Trs, Tr)
+    ).
+
+
+% http://stackoverflow.com/questions/8519203/prolog-replace-an-element-in-a-list-at-a-specified-index
+replace([_|T], 0, X, [X|T]).
+replace([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace(T, NI, X, R), !.
+replace(L, _, _, L).
+
+writeSymbol(T, S, A, R) :-
+    nth0(I, T, S),
+    J is I + 1,
+    replace(T, J, A, R).
+
+% check na abnormalni zastaveni
+shiftLeft(T, S, R) :- 
+    nth0(I, T, S),
+    J is I - 1,
+    nth0(J, T, Tmp),
+    replace(T, J, S, X),
+    replace(X, I, Tmp, R).   
+
+% check na blank
+shiftRight(T, S, R) :- 
+    nth0(I, T, S),
+    J is I + 1,
+    nth0(J, T, Tmp),
+    replace(T, J, S, X),
+    replace(X, I, Tmp, R).   
 
 main :- 
     prompt(_,''),
     readLines(LL),
     parseLines(LL, LP),
-    write(LP),
+    last(LP,T),
+    delete(LP,T,Trs),
+    append(['S'], T, ST),
+    write(ST),
+    simulateTS(ST, 'S', Trs, R),
     halt.
+
 
 
 % vim: expandtab:shiftwidth=4:tabstop=4:softtabstop=0:textwidth=120
