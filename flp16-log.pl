3483f06e4849fa3c86f25ec6345a550cc0bdf3cd
Tomas Bruckner <tomasbruckner@seznam.cz>
2016-04-27 17:55:02 +0200

    chystani na odevzdani

--- flp16-log.pl
+++ flp16-log.pl
@@ -2,7 +2,27 @@
 % Course:  Funcional and Logical Programming
 % Author:  Tomas Bruckner, xbruck02@stud.fit.vutbr.cz
 % Date:	   2016-04-26
-%
+
+
+% Starting point, parses Turing Machine from stdin
+% TS transition format:
+% <state><space><symbol><space><newstate><space><symbol/L/R>
+% TS tape format:
+% <symbol><symbol><symbol>...
+main :-
+    prompt(_,''),
+    readLines(Lines),
+    last(Lines, Tape),
+    delete(Lines, Tape, PreparsedTransitions),
+    parseTransitions(PreparsedTransitions, Transitions),
+    append(['S'], Tape, StartingTape),
+    !,
+    ( simulateTS(StartingTape, 'S', Transitions, Configurations);
+      writef("Abnormal halt!\n"), halt
+    ),
+    append([StartingTape], Configurations, FinalConfigurations),
+    writeConfigurations(FinalConfigurations),
+    halt.
 
 % Reads every line from stdin
 readLines(Lines) :-
@@ -25,7 +45,7 @@ isEOFEOL(Char) :-
 % Parses every lines to transition format
 parseTransitions([],[]).
 parseTransitions([Line|RestLines], [ParsedLine|ParsedLines]) :-
-    parseTransitions(RestLines,ParsedLines), parseTransition(Line,ParsedLine).
+    parseTransition(Line,ParsedLine), parseTransitions(RestLines,ParsedLines).
 
 % Parses single line
 % transition "oldstate symbol newstate action"
@@ -38,8 +58,9 @@ parseTransition(Line,Parsed) :-
 
 % Simulates Turing Machine
 simulateTS(Tape, State, Transitions, Output) :-
+    % final state
     State == 'F', Output = [];
-
+    % not in final state
     getTransition(Tape, State, Transitions, SingleTransition),
     nth0(2, SingleTransition, NewState),
     nth0(3, SingleTransition, Action),
@@ -49,6 +70,7 @@ simulateTS(Tape, State, Transitions, Output) :-
     ),
     changeState(NewTape, State, NewState, FNewTape),
     simulateTS(FNewTape, NewState, Transitions, RestOutput),
+    % all configurations
     Output = [FNewTape|RestOutput].
 
 % Gets transition for current state and symbol on head
@@ -56,33 +78,14 @@ getTransition(Tape, State, [CurrentTransition|Transitions], OutputTransition) :-
     nth0(StateIndex, Tape, State),
     SymbolIndex is StateIndex + 1,
     nth0(SymbolIndex, Tape, Symbol),
-    (nth0(0, CurrentTransition, TransitionState),
-     TransitionState == State,
-     nth0(1, CurrentTransition, TransitionSymbol),
-     TransitionSymbol == Symbol,
-     OutputTransition = CurrentTransition ;
-
+    ( nth0(0, CurrentTransition, TransitionState),
+      TransitionState == State,
+      nth0(1, CurrentTransition, TransitionSymbol),
+      TransitionSymbol == Symbol,
+      OutputTransition = CurrentTransition ;
+     % CurrentTransition is not valid for state and symbol under head, we must continue searching
      getTransition(Tape, State, Transitions, OutputTransition)
     ).
-
-% Changes state on tape
-changeState(Tape, OldState, NewState, NewTape) :-
-    nth0(StateIndex, Tape, OldState), replace(Tape, StateIndex, NewState, NewTape).
-
-% Replaces element in list at specified index
-% http://stackoverflow.com/questions/8519203/prolog-replace-an-element-in-a-list-at-a-specified-index
-replace([_|RestElements], 0, NewElement, [NewElement|RestElements]).
-replace([FirstElement|RestOriginalElements], Index, NewElement, [FirstElement|RestElements]):- 
-    Index > -1,
-    NewIndex is Index - 1,
-    replace(RestOriginalElements, NewIndex, NewElement, RestElements), !.
-replace(List, _, _, List).
-
-% Writes speicified symbol on tape at head location
-writeSymbol(Tape, State, Symbol, NewTape) :-
-    nth0(StateIndex, Tape, State),
-    SymbolIndex is StateIndex + 1,
-    replace(Tape, SymbolIndex, Symbol, NewTape).
 
 % Shifts head to the left
 shiftLeft(Tape, State, NewTape) :-
@@ -90,8 +93,7 @@ shiftLeft(Tape, State, NewTape) :-
     NewStateIndex is StateIndex - 1,
     nth0(NewStateIndex, Tape, Tmp),
     replace(Tape, NewStateIndex, State, TmpTape),
-    replace(TmpTape, StateIndex, Tmp, NewTape)
-    .
+    replace(TmpTape, StateIndex, Tmp, NewTape).
 
 % Shifts head to the right
 shiftRight(Tape, State, NewTape) :-
@@ -99,37 +101,35 @@ shiftRight(Tape, State, NewTape) :-
     NewStateIndex is StateIndex + 1,
     length(Tape, Len),
     HighestIndex is Len - 1,
-    (NewStateIndex == HighestIndex, append(Tape, [' '], TmpTape);
-     TmpTape = Tape
+    ( NewStateIndex == HighestIndex, append(Tape, [' '], TmpTape);
+      TmpTape = Tape
     ),
     nth0(NewStateIndex, TmpTape, Tmp),
     replace(TmpTape, NewStateIndex, State, TmpTape2),
     replace(TmpTape2, StateIndex, Tmp, NewTape).
+
+% Writes speicified symbol on tape at head location
+writeSymbol(Tape, State, Symbol, NewTape) :-
+    nth0(StateIndex, Tape, State),
+    SymbolIndex is StateIndex + 1,
+    replace(Tape, SymbolIndex, Symbol, NewTape).
+
+% Changes state on tape
+changeState(Tape, OldState, NewState, NewTape) :-
+    nth0(StateIndex, Tape, OldState), replace(Tape, StateIndex, NewState, NewTape).
+
+% Replaces element in list at specified index
+replace([_|RestElements], 0, NewElement, [NewElement|RestElements]).
+replace([FirstElement|RestOriginalElements], Index, NewElement, [FirstElement|RestElements]):-
+    Index > -1,
+    NewIndex is Index - 1,
+    replace(RestOriginalElements, NewIndex, NewElement, RestElements), !.
+replace(List, _, _, List).
 
 % Writes configurations after simmulation
 writeConfigurations([]).
 writeConfigurations([Configuration|RestConfigurations]) :-
     Configuration == [] ;
     writef("%s\n", [Configuration]), writeConfigurations(RestConfigurations).
-
-% Starting point, parses Turing Machine from stdin
-% TS transition format:
-% <state><space><symbol><space><newstate><space><symbol/L/R>
-% TS tape format:
-% <symbol><symbol><symbol>...
-main :-
-    prompt(_,''),
-    readLines(Lines),
-    last(Lines, Tape),
-    delete(Lines, Tape, PreparsedTransitions), 
-    parseTransitions(PreparsedTransitions, Transitions),
-    append(['S'], Tape, StartingTape),
-    !,
-    ( simulateTS(StartingTape, 'S', Transitions, Configurations);
-      writef("Abnormal halt!\n"), halt
-    ),
-    append([StartingTape], Configurations, FinalConfigurations),
-    writeConfigurations(FinalConfigurations),
-    halt.
 
 % vim: expandtab:shiftwidth=4:tabstop=4:softtabstop=0:textwidth=120
