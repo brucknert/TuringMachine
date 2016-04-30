4f06cac264475baaf46e04c9f99420af6ec5135b
Tomas Bruckner <xbruck02@stud.fit.vutbr.cz>
2016-04-30 11:49:32 +0200

    opravy testu, fungujici vsechny testy

--- flp16-log.pl
+++ flp16-log.pl
@@ -25,20 +25,21 @@ main :-
 % Reads every line from stdin
 readLines(Lines) :-
     readLine(Line,Char),
-    ( isEOFEOL(Char), Lines = [];
-    readLines(RestLines), Lines = [Line|RestLines]).
+    ( isEOFEOL(Char), Lines = []
+      ; readLines(RestLines), Lines = [Line|RestLines]
+    ).
 
 % Reads single line
 readLine(Line, Char) :-
     get_char(Char),
-    (isEOFEOL(Char), Line = [], !;
-     readLine(RestChars,_), [Char|RestChars] = Line
+    ( isEOFEOL(Char), Line = [], !
+      ; readLine(RestChars,_), [Char|RestChars] = Line
     ).
 
 % Checks for EOF or EOL
 isEOFEOL(Char) :-
-    Char == end_of_file;
-    (char_code(Char,Code), Code == 10).
+    Char == end_of_file
+    ; (char_code(Char,Code), Code == 10).
 
 % Parses every lines to transition format
 parseTransitions([],[]).
@@ -46,12 +47,7 @@ parseTransitions([Line|RestLines], [ParsedLine|ParsedLines]) :-
     parseTransition(Line,ParsedLine), parseTransitions(RestLines,ParsedLines).
 
 % Parses single line
-% transition "oldstate symbol newstate action"
-parseTransition(Line,Parsed) :-
-    nth0(0, Line, OldState),
-    nth0(2, Line, Symbol),
-    nth0(4, Line, NewState),
-    nth0(6, Line, Action),
+parseTransition([OldState, ' ', Symbol, ' ', NewState, ' ', Action | []],Parsed) :-
     Parsed = [OldState, Symbol, NewState, Action].
 
 % Indicates if we should continue with next iteration of Iterative Depth Search
@@ -59,29 +55,24 @@ parseTransition(Line,Parsed) :-
 
 % Iterative Depth Search
 ids(Level, Tape, State, Transitions, Output) :- 
-    % Final state found in specified depth
-    simulateTS(Level, Tape, State, Transitions, Output); 
-    % Final state can still be in higher depth
-    continue, retract(continue), NewLevel is Level + 1, ids(NewLevel, Tape, State, Transitions, Output);
-    % There are no valid transitions
-    writef("Abnormal halt!\n"), halt.
+    simulateTS(Level, Tape, State, Transitions, Output)
+    ; continue, retract(continue), NewLevel is Level + 1, ids(NewLevel, Tape, State, Transitions, Output)
+    ; writef("Abnormal halt!\n"), halt.
 
 % Simulates Turing Machine
 simulateTS(0, _, S, _, Output) :- asserta(continue), S == 'F', Output = [], !.
 simulateTS(Level, Tape, State, Transitions, Output) :-
-    % not in final state
     Level > 0,
     getTransition(Tape, State, Transitions, SingleTransition),
     nth0(2, SingleTransition, NewState),
     nth0(3, SingleTransition, Action),
-    ( Action == 'L', shiftLeft(Tape, State, NewTape);
-      Action == 'R', shiftRight(Tape, State, NewTape);
-      writeSymbol(Tape, State, Action, NewTape) 
+    ( Action == 'L', shiftLeft(Tape, State, NewTape)
+      ; Action == 'R', shiftRight(Tape, State, NewTape)
+      ; Action \= 'L', Action \= 'R', writeSymbol(Tape, State, Action, NewTape) 
     ),
     changeState(NewTape, State, NewState, FNewTape),
     NewLevel is Level - 1,
     simulateTS(NewLevel, FNewTape, NewState, Transitions, RestOutput),
-    % all configurations
     Output = [FNewTape|RestOutput].
 
 % Gets transition for current state and symbol on head
@@ -94,9 +85,9 @@ getTransition(Tape, State, [CurrentTransition|Transitions], OutputTransition) :-
       TransitionState == State,
       nth0(1, CurrentTransition, TransitionSymbol),
       TransitionSymbol == Symbol,
-      OutputTransition = CurrentTransition ;
-     % CurrentTransition is not valid for state and symbol under head, we must continue searching
-     getTransition(Tape, State, Transitions, OutputTransition)
+      OutputTransition = CurrentTransition
+      % CurrentTransition is not valid for state and symbol under head, we must continue searching
+      ; getTransition(Tape, State, Transitions, OutputTransition)
     ).
 
 % Shifts head to the left
@@ -113,14 +104,14 @@ shiftRight(Tape, State, NewTape) :-
     NewStateIndex is StateIndex + 1,
     length(Tape, Len),
     HighestIndex is Len - 1,
-    ( NewStateIndex == HighestIndex, append(Tape, [' '], TmpTape);
-      TmpTape = Tape
+    ( NewStateIndex == HighestIndex, append(Tape, [' '], TmpTape)
+      ; TmpTape = Tape
     ),
     nth0(NewStateIndex, TmpTape, Tmp),
     replace(TmpTape, NewStateIndex, State, TmpTape2),
     replace(TmpTape2, StateIndex, Tmp, NewTape).
 
-% Writes speicified symbol on tape at head location
+% Writes specified symbol on tape at head location
 writeSymbol(Tape, State, Symbol, NewTape) :-
     nth0(StateIndex, Tape, State),
     SymbolIndex is StateIndex + 1,
@@ -133,15 +124,14 @@ changeState(Tape, OldState, NewState, NewTape) :-
 % Replaces element in list at specified index
 replace([_|RestElements], 0, NewElement, [NewElement|RestElements]).
 replace([FirstElement|RestOriginalElements], Index, NewElement, [FirstElement|RestElements]):-
-    Index > -1,
+    Index > 0,
     NewIndex is Index - 1,
     replace(RestOriginalElements, NewIndex, NewElement, RestElements), !.
-replace(List, _, _, List).
 
 % Writes configurations after simmulation
 writeConfigurations([]).
 writeConfigurations([Configuration|RestConfigurations]) :-
-    Configuration == [] ;
-    writef("%s\n", [Configuration]), writeConfigurations(RestConfigurations).
+    Configuration == []
+    ; writef("%s\n", [Configuration]), writeConfigurations(RestConfigurations).
 
 % vim: expandtab:shiftwidth=4:tabstop=4:softtabstop=0:textwidth=120
